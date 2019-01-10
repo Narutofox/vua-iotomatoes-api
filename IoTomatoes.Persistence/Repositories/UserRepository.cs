77f6f02eaeade1be9fb07e412656ad8a82c2f482
Ivan Dumančić <ivan.dumancic1995@gmail.com>
2019-01-10 20:15:01 +0100

    Add generic and user repository

---
+++ IoTomatoes.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using IoTomatoes.Domain.Interfaces;
+using IoTomatoes.Domain.Models;
+using IoTomatoes.Persistence.Commons;
+
+namespace IoTomatoes.Persistence.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        public UserRepository(AppContext context) : base(context) {}
+    }
+}
